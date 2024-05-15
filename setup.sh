a0075dd31593201b52dcfbdd73a0ec23120dcc08
guanz808 <jiwanaga@hotmail.com>
2024-05-15 09:28:54 -1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,8 @@ apt update -y
 apt install ansible -y
 ansible --version
 
-cd /etc/ansible
-sudo curl -o ansible.cfg https://raw.githubusercontent.com/guanz808/ansible/master/ansible.cfg
-# Review ansible.cfg content
-sudo curl -o base.yml https://raw.githubusercontent.com/guanz808/ansible/master/base.yml
-# Review base.yml content
-sudo curl -o hosts https://raw.githubusercontent.com/guanz808/ansible/master/hosts
-# Review hosts content
+curl -o ansible.cfg https://raw.githubusercontent.com/guanz808/ansible/master/ansible.cfg -o /etc/ansible
+curl -o base.yml https://raw.githubusercontent.com/guanz808/ansible/master/base.yml -o /etc/ansible
+curl -o hosts https://raw.githubusercontent.com/guanz808/ansible/master/hosts -o /etc/ansible
 
 ansible-playbook /etc/ansible/base.yml
