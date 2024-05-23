14bb7728e7fe7f39415688129fd5d6714aadee2f
guanz808 <jiwanaga@hotmail.com>
2024-05-22 18:04:06 -1000

    add logic to sethp.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && apt upgrade -y
+
 sudo apt -y install \
 - software-properties-common \
 - git \
@@ -7,6 +7,34 @@ sudo apt -y install \
 - sudo \
 - adduser \
 - vim 
+
+#!/bin/bash
+
+sudo apt update && apt upgrade -y
+
+# Check if we're in a container
+if [ -f /.dockerenv ]; then
+  # Container environment, use sudo for installation
+  echo "Detected container environment. Using sudo for installation."
+  apt-get update && apt-get install -y \
+    software-properties-common \
+    curl \
+    git \
+    vim \
+    adduser \
+    sudo \
+    locales
+else
+  # Not a container, use regular apt-get
+  echo "Not detected as a container environment. Skipping sudo for installation."
+  apt-get update && apt-get install -y \
+    software-properties-common \
+    curl \
+    git \
+    vim
+fi
+
+
 
 sudo apt-add-repository -y ppa:ansible/ansible -y
 sudo apt-get update -y
