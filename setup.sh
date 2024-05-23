52230b2fbd983b6853982a6a7c0401525e8264da
guanz808 <jiwanaga@hotmail.com>
2024-05-22 17:48:03 -1000

    update

--- setup.sh
+++ setup.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
-#apt update && apt upgrade -y
-#apt -y install software-properties-common curl sudo adduser vim locales git 
+sudo apt update && apt upgrade -y
+sudo apt -y install \
+- software-properties-common \
+- git \
+- curl \
+- sudo \
+- adduser \
+- vim 
 
-function ubuntu_setup() {
-  if ! dpkg -s ansible >/dev/null 2>&1; then
-    _task "Installing Ansible"
-    _cmd "sudo apt-get update"
-    _cmd "sudo apt-get install -y software-properties-common"
-    _cmd "sudo apt-add-repository -y ppa:ansible/ansible"
-    _cmd "sudo apt-get update"
-    _cmd "sudo apt-get install -y ansible"
-    _cmd "sudo apt-get install python3-argcomplete"
-    _cmd "sudo activate-global-python-argcomplete3"
-  fi
-  if ! dpkg -s python3 >/dev/null 2>&1; then
-    _task "Installing Python3"
-    _cmd "sudo apt-get install -y python3"
-  fi
-  if ! dpkg -s python3-pip >/dev/null 2>&1; then
-    _task "Installing Python3 Pip"
-    _cmd "sudo apt-get install -y python3-pip"
-  fi
-  if ! pip3 list | grep watchdog >/dev/null 2>&1; then
-    _task "Installing Python3 Watchdog"
-    _cmd "pip3 install watchdog"
-  fi
-}
+sudo apt-add-repository -y ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt install -y ansible
