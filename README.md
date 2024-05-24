50727144ef11d89582ecba76bdf62edd5facad51
guanz808 <jiwanaga@hotmail.com>
2024-05-23 18:23:24 -1000

    update

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-apt update && apt upgrade -y
-apt install git -y
+# Check if we're in a container
+if [ -f /.dockerenv ]; then
+  # Container environment, use sudo for installation
+  echo "Detected container environment. Using sudo for installation."
+  apt-get update && apt-get install -y \
+    software-properties-common \
+    git \
+    vim \
+    adduser \
+    sudo \
+    locales
+
+  echo "change root password"
+  passwd root
+
+else
+  # Not a container, use regular apt-get
+  echo "Not detected as a container environment. Skipping sudo for installation."
+  apt-get update && apt-get install -y \
+    software-properties-common \
+    git \
+    vim
+fi
+
+echo "creating user jiadmin account"
+adduser jiadmin
+usermod -aG sudo jiadmin
+su jiadmin
+
 cd ~
 git clone https://github.com/guanz808/ansible.git
 cd ~/ansible
 
 chmod + ./setup.sh
 # if running in a container run
-bash ./setup.sh
+sudo bash ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-sudo apt update && apt upgrade -y
-
-# Check if we're in a container
-if [ -f /.dockerenv ]; then
-  # Container environment, use sudo for installation
-  echo "Detected container environment. Using sudo for installation."
-  apt-get update && apt-get install -y \
-    software-properties-common \
-    curl \
-    git \
-    vim \
-    adduser \
-    sudo \
-    locales
-else
-  # Not a container, use regular apt-get
-  echo "Not detected as a container environment. Skipping sudo for installation."
-  apt-get update && apt-get install -y \
-    software-properties-common \
-    curl \
-    git \
-    vim
-fi
-
-passwd root
-adduser jiadmin
-usermod -aG sudo jiadmin
-su jiadmin
-
-
 sudo apt-add-repository -y ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt install -y ansible
@@ -37,4 +7,6 @@ sudo apt install -y ansible
 sudo locale-gen "en_US.UTF-8"
 
 cd ~/ansible
-ansible-playbook main.yml --become
+echo "Add the key to the .vault_key file"
+
+#ansible-playbook main.yml --become
