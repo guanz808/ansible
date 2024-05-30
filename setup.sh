754753eaf00292f5241904a4b15cce1c972d7aec
guanz808 <jiwanaga@hotmail.com>
2024-05-30 11:56:25 -1000

    e

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 # Prompt for Key
 read -p "Enter the vault key: " key
 
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
+ansible --version
+
 if [ ! -d ~/ansible ]; then
   # Directory doesn't exist, clone the repository
   echo "Directory ~/ansible not found. Cloning the repository..."
@@ -24,10 +29,10 @@ echo "$key" > ~/ansible/.vault_key
 cat ~/ansible/.vault_key
 
 # Run the setup script (use sudo if in a container)
-#if [ -f /.dockerenv ]; then
-#  # Run the Ansible playbook
-#  sudo ansible-playbook main.yml --become
-#else
-#  # Run the Ansible playbook
-#  ansible-playbook main.yml --become
-#fi
+if [ -f /.dockerenv ]; then
+  # Run the Ansible playbook
+  sudo ansible-playbook main.yml --become
+else
+  # Run the Ansible playbook
+  ansible-playbook main.yml --become
+fi
