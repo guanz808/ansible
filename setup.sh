db45d1c1321ce2c943abfb42f620ffc75af60998
guanz808 <jiwanaga@hotmail.com>
2024-05-30 12:04:52 -1000

    2

--- setup.sh
+++ setup.sh
@@ -17,22 +17,21 @@ if [ ! -d ~/ansible ]; then
 else
   # Directory exists, handle existing content (optional)
   echo "Directory ~/ansible already exists."
-  pwd
   cd ~/ansible
-  ll
   git reset --hard origin/main
-  # You can add logic here to handle existing content (e.g., prompt user)
 fi
 
 # Add the key to the vault_key file
 echo "$key" > ~/ansible/.vault_key
 cat ~/ansible/.vault_key
 
+ansible-playbook main.yml --become
+
 # Run the setup script (use sudo if in a container)
-if [ -f /.dockerenv ]; then
-  # Run the Ansible playbook
-  sudo ansible-playbook main.yml --become
-else
-  # Run the Ansible playbook
-  ansible-playbook main.yml --become
-fi
+#if [ -f /.dockerenv ]; then
+#  # Run the Ansible playbook
+#  sudo ansible-playbook main.yml --become
+#else
+#  # Run the Ansible playbook
+#  ansible-playbook main.yml --become
+#fi
