37845dca2a13a1073a502060f95a569209951d6e
guanz808 <jiwanaga@hotmail.com>
2024-05-30 10:54:51 -1000

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Clone the Ansible repository
-cd ~
-git clone https://github.com/guanz808/ansible.git
-cd ~/ansible
+# Prompt for Key
+read -p "Enter the vault key: " key
 
-# Make the setup script executable
-chmod +x ./setup.sh
-
-# Run the setup script (use sudo if in a container)
-if [ -f /.dockerenv ]; then
-  sudo bash ./setup.sh
-else
-  bash ./setup.sh
+if [ ! -d ~/ansible ]; then
+  # Command to run if the directory doesn't exist
+  echo "Directory ~/ansible not found. Running specific command..."
+  cd ~
+  git clone https://github.com/guanz808/ansible.git
+  cd ~/ansible
 fi
 
 # Add the key to the vault_key file (replace "key" with your actual key)
-echo "key" > ~/ansible/.vault_key
+echo "$key" > ~/ansible/.vault_key
 cat ~/ansible/.vault_key
 
-# Run the Ansible playbook
-ansible-playbook main.yml --become
+# Run the setup script (use sudo if in a container)
+if [ -f /.dockerenv ]; then
+  # Run the Ansible playbook
+  sudo ansible-playbook main.yml --become
+else
+  # Run the Ansible playbook
+  ansible-playbook main.yml --become
+fi
