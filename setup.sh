7a6feb9e50d5f1ab42ce0921be2466d3b9816e3e
guanz808 <jiwanaga@hotmail.com>
2024-05-30 17:11:46 -1000

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Define color codes as variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'  # Reset color to default
+
+# install ansible
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update -y
 sudo apt install ansible -y
