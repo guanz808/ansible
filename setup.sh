a33d1f1ae74bf14a3ac03beae86e3ba94baff9ad
guanz808 <jiwanaga@hotmail.com>
2024-05-30 16:24:23 -1000

    uncomment

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ cat ~/ansible/.vault_key
 # Add the key to the vault_key file
 #echo "$key" > ~/ansible/.vault_key
 
-#ansible-playbook main.yml --become
+ansible-playbook main.yml --become
 
 # Run the setup script (use sudo if in a container)
 #if [ -f /.dockerenv ]; then
